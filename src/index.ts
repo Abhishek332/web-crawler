0b01bb1120dcceb6f24d125415b4697f0a989bf4
abhishek332 <itsabhiofficial.21@gmail.com>
2022-09-16 22:48:46 +0530

    Crawl all links from the page is complete

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
+import * as cheerio from "cheerio";
 
 const crawler = async () => {
     try {
-        let response = await axios.get("https://abhishek332.github.io/Fresh-Meals/");
-        console.log(response);
+        let { data } = await axios.get("https://abhishek332.github.io/Fresh-Meals/");
+        let $html = cheerio.load(data);
+        const links = $html('a')
+            .map((i, a) => a.attribs.href).get();
+        console.log(links);
     } catch (error) {
         console.log("Error : ", error);
     }
